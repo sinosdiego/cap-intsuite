b4b3c56b9f59c28f755c4a26e421da56a2c4eef1
Diego Dapont <diego.d@sinosbyte.com.br>
2023-08-17 13:52:03 -0300

    teste2

--- srv/client.js
+++ srv/client.js
@@ -50,21 +50,21 @@ module.exports = (endpoint) => {
 
         console.log(`[INFO] Credentials Ok`);
 
-        try {
-            //invoke integration flow
-            const response = await axios.post(config.targetUrl, message, {
-                headers: {
-                    "Authorization": `Bearer ${authResponse.access_token}`,
-                    "Content-Type": "text/plain"
-                },
-                httpsAgent: targetAgent
-            });
+        // try {
+        //     //invoke integration flow
+        //     const response = await axios.post(config.targetUrl, message, {
+        //         headers: {
+        //             "Authorization": `Bearer ${authResponse.access_token}`,
+        //             "Content-Type": "text/plain"
+        //         },
+        //         httpsAgent: targetAgent
+        //     });
 
-            console.log(`[INFO] Successfully invoked integration flow: Received response "${response}"`);
+        //     console.log(`[INFO] Successfully invoked integration flow: Received response "${response}"`);
 
-        } catch (error) {
-            console.log(`[ERROR] Failed: ${error}`);
-        }
+        // } catch (error) {
+        //     console.log(`[ERROR] Failed: ${error}`);
+        // }
     });
 
     //OData functions
