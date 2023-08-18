dcac3b6dc5bb3144a950615b8fa0baa0b25045bb
Diego Dapont <diego.d@sinosbyte.com.br>
2023-08-18 13:03:55 -0300

    .

---
+++ creds-local.js
@@ -0,0 +1,12 @@
+module.exports = {
+    //mesme nome utilizado no client.js, função executada no evento "message"
+    //acho que o nome deveria ser o nome da instancia do CredentialStore criado ou do namespace    
+    "integration-suite": {
+        username: "sb-d9ccaea5-527d-48fc-b364-3c9fa65b7e2d!b173480|it-rt-674dd9b3trial!b26655",
+        password: "fb07ba34-21e9-40ae-baeb-51bce4f7123a$SRk6Z6Z0btmhbrW-f6FORohz4qYrX4FcHWYO6dE0UNo="
+    },
+    "cap-intsuite": {
+        username: "sb-d9ccaea5-527d-48fc-b364-3c9fa65b7e2d!b173480|it-rt-674dd9b3trial!b26655",
+        password: "fb07ba34-21e9-40ae-baeb-51bce4f7123a$SRk6Z6Z0btmhbrW-f6FORohz4qYrX4FcHWYO6dE0UNo="
+    }
+}
