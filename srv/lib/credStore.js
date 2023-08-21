baf56e3c988330767d836b0695496415791e29b0
Diego Dapont <diego.d@sinosbyte.com.br>
2023-08-21 13:29:41 -0300

    .

--- srv/lib/credStore.js
+++ srv/lib/credStore.js
@@ -23,8 +23,6 @@ function checkStatus(response) {
 }
 
 async function decryptPayload(privateKey, payload) {
-    console.log(`[INFODD] decryptPayload privateKey ${privateKey}`);
-    console.log(`[INFODD] decryptPayload payload ${payload}`);
     const key = await jose.JWK.asKey(`-----BEGIN PRIVATE KEY-----${privateKey}-----END PRIVATE KEY-----`, "pem", {
         alg: "RSA-OAEP-256",
         enc: "A256GCM",
@@ -37,13 +35,13 @@ async function decryptPayload(privateKey, payload) {
 function headers(binding, namespace, init) {
     const result = new fetch.Headers(init);
     result.set("Authorization", `Basic ${Buffer.from(`${binding.username}:${binding.password}`).toString("base64")}`);
-    // result.set("Authorization", `Basic ${Buffer.from(`${binding.username}`).toString("base64")}`);
     result.set("sapcp-credstore-namespace", namespace);
     console.log(`[INFODD] headers result ${result}`);
     return result;
 }
 
 async function fetchAndDecrypt(privateKey, url, method, headers, cert, key, body) {
+    // para uso de MTL, que passa a chave e o certificado, sem o header Autorization
     // const result = await fetch(url, { method, headers, agentOptions: { cert, key }, body })
     const result = await fetch(url, { method, headers, body })
         .then(checkStatus)
@@ -62,21 +60,20 @@ async function fetchAndDecrypt(privateKey, url, method, headers, cert, key, body
  * @returns Credential retrieved from the credential store
  */
 async function readCredential(binding, namespace, type, name) {
-    console.log(`[INFODD] readCredential private_key ${binding.encryption.client_private_key}`);
-    console.log(`[INFODD] readCredential url ${binding.url}`);
-    console.log(`[INFODD] readCredential username ${binding.username}`);
-    console.log(`[INFODD] readCredential password ${binding.password}`);
-    console.log(`[INFODD] readCredential type ${type}`);
-    console.log(`[INFODD] readCredential header ${Buffer.from(`${binding.username}:${binding.password}`).toString("base64")}`);
-    console.log(`[INFODD] readCredential name ${encodeURIComponent(name)}`);
-    // console.log(`[INFODD] readCredential body ${util.inspect(headers(binding, namespace), false, null, true)}`);
+    // console.log(`[INFODD] readCredential private_key ${binding.encryption.client_private_key}`);
+    // console.log(`[INFODD] readCredential url ${binding.url}`);
+    // console.log(`[INFODD] readCredential username ${binding.username}`);
+    // console.log(`[INFODD] readCredential password ${binding.password}`);
+    // console.log(`[INFODD] readCredential type ${type}`);
+    // console.log(`[INFODD] readCredential header ${Buffer.from(`${binding.username}:${binding.password}`).toString("base64")}`);
+    // console.log(`[INFODD] readCredential name ${encodeURIComponent(name)}`);
 
     return fetchAndDecrypt(
         binding.encryption.client_private_key,
         `${binding.url}/${type}?name=${encodeURIComponent(name)}`,
         "get",
         headers(binding, namespace),
-        binding.certificate,
-        binding.key
+        binding.certificate,    //utilizado no MTLs
+        binding.key             //utilizado no MTLs
     );
 }
